44fd5fb33c6052cea67401b0b7962726bdfc62e6
Nikoloz Kavtaradze <n.kavtaradze94@gmail.com>
2015-12-14 01:06:39 +0000

    Add CRUD to Student

--- University-BL-DAL/BL/Student.cs
+++ University-BL-DAL/BL/Student.cs
@@ -23,5 +23,32 @@ namespace University_BL_DAL.BL
             this.Gpa = gpa;
             this.Email = email;
         }
+
+        public static IEnumerable<Student> GetAll()
+        {
+            return (StudentDAL.GetAll());
+        }
+
+        public static Student Find(long id)
+        {
+            return (StudentDAL.Find(id));
+        }
+
+        public bool Create()
+        {
+            bool res = false;
+            res = StudentDAL.Create(this);
+            return (res);
+        }
+
+        public bool Update()
+        {
+            return (StudentDAL.Update(this));
+        }
+
+        public bool Delete()
+        {
+            return (StudentDAL.Delete(this));
+        }
     }
 }
